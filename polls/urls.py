814c6f6efcd883cf9fe91e7471deece31e0f763e
Lone <lone0059@gmail.com>
2022-09-09 15:06:49 +0500

    Revising urlpatterns & ORM Models

--- polls/urls.py
+++ polls/urls.py
@@ -1,8 +1,25 @@
+from unittest import result
 from django.urls import path
 from . import views
 
 #Creating URL Patterns To Call App View's
 
+
+
+app_name = 'polls'
 urlpatterns = [
+    # Example :/polls/
     path('', views.index, name="index"),
+
+    # Example; polls/5
+    path('<int:question_id>/', views.detail, name='detail'),
+
+    # Example: polls/5/results
+    path('<int:question_id>/results/', views.results, name='results'),
+
+    # Example: polls/5/vote
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+
+
 ] 
+
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,30 @@
-from django.http import HttpResponse
+from gc import get_objects
+from multiprocessing import context
+from urllib import response
+from django.http import HttpResponse, Http404
+from django.template import loader
+from django.shortcuts import render, get_object_or_404
+
+from .models import Question
 
 
-def index(Response):
-    return HttpResponse("This is me, revising django again")
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = { 'latest_question_list': latest_question_list }
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question' : question})
+
+
+def results(request, question_id):
+    response = f"You're looking at the results of question {question_id}"
+    return HttpResponse(response)
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting question {question_id}")
+
+
 
